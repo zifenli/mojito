59ca9b5ed249625eaae7e4d96e73d5e69eaed661
lizifen <zifenli@gmail.com>
2018-06-22 19:01:08 +0800

    添加pm2config

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'mojito',
+      script: 'server/app.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '140.143.187.20',
+      ref: 'origin/master',
+      repo: 'git@github.com:zifenli/mojito.git',
+      path: '/home/ubuntu/projects/mojito/',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
