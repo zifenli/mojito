17bfcf70cc76c0c947acafb7cd9b21dee9b5325c
lizifen <zifenli@gmail.com>
2018-07-10 23:48:44 +0800

    整理页面结构

--- src/router.js
+++ src/router.js
@@ -1,15 +1,33 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
+import Components from './views/Components.vue';
+import BugKiller from './components/BugKiller.vue';
+import Reporter from './components/Reporter.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/components',
+      name: 'components',
+      component: Components,
+      children: [
+        {
+          path: 'bug-killer',
+          component: BugKiller,
+        },
+        {
+          path: 'reporter',
+          component: Reporter,
+        },
+      ],
     },
   ],
 });
---
+++ src/views/Components.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Menu class="mj-menu" :theme="theme3" active-name="1">
+      <Menu-item name="bug-killer">
+        <router-link to="/dashboard">
+          <Icon type="paper-airplane"></Icon>
+          &nbsp;&nbsp;dashboard
+        </router-link>
+      </Menu-item>
+      <Submenu name="components">
+        <template slot="title">
+          <Icon type="leaf"></Icon>
+          组件库
+        </template>
+        <Menu-item name="bug-killer">
+          <router-link to="/components/bug-killer">
+            <Icon type="chatbubble-working"></Icon>
+            &nbsp;&nbsp;Tower消息
+          </router-link>
+        </Menu-item>
+        <Menu-item name="reporter">
+          <router-link to="/components/reporter">
+            <Icon type="ios-grid-view"></Icon>
+            &nbsp;&nbsp;数据报表
+          </router-link>
+        </Menu-item>
+      </Submenu>
+    </Menu>
+    <div class="mi-content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import { Menu, MenuGroup, MenuItem, Icon, Submenu } from 'iview';
+import ChartDisplay from '@/components/ChartDisplay.vue';
+
+export default {
+  data() {
+    return {
+      theme3: 'dark',
+    };
+  },
+  components: {
+    Menu, MenuGroup, MenuItem, Icon, Submenu, ChartDisplay,
+  },
+};
+</script>
+<style lang="less">
+  .mj-menu{
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    height: 100%;
+    background-color: #495060;
+    a{
+      color: white;
+    }
+  }
+  .mi-content{
+    margin-left: 240px;
+  }
+</style>
